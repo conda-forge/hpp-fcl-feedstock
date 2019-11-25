17b69ce3bb2f8a5358a246bbfd9c3e4f33b874e0
Justin Carpentier <justin.carpentier@inria.fr>
2019-11-25 11:51:39 +0100

    Enable Python bindings

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ cd build
 
 cmake .. \
       -DCMAKE_BUILD_TYPE=Release \
+      -DBUILD_PYTHON_INTERFACE=ON \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=lib
