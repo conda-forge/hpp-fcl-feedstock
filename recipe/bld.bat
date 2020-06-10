4aa1fdf040d670754094004a14d0d89b9b11fa3b
Justin Carpentier <justin.carpentier@inria.fr>
2020-06-10 23:09:01 +0200

    win: add build script

---
+++ recipe/bld.bat
@@ -0,0 +1,22 @@
+set PKG_CONFIG_PATH=%LIBRARY_PREFIX%\share\pkgconfig
+
+mkdir build
+cd build
+
+cmake ^
+    -G "Visual Studio" -T "ClangCl" -DCMAKE_GENERATOR_PLATFORM=x64 ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DHPP_FCL_SITELIB_ROOT=%PREFIX% ^
+    -DPYTHON_EXECUTABLE=%PYTHON% ^
+    -DBUILD_PYTHON_INTERFACE=ON ^
+    %SRC_DIR%
+if errorlevel 1 exit 1
+
+:: Build.
+cmake --build . --config Release
+if errorlevel 1 exit 1
+
+:: Install.
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
