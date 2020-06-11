0c03f48f9b6deaf7a8c37de530d0a9a6352ba479
Justin Carpentier <justin.carpentier@inria.fr>
2020-06-11 06:50:11 +0200

    bld: fix generators

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,7 +4,7 @@ mkdir build
 cd build
 
 cmake ^
-    -G %CMAKE_GEN% -T "ClangCl" ^
+    -G "%CMAKE_GEN%" -T "ClangCl" ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DHPP_FCL_SITELIB_ROOT=%PREFIX% ^
