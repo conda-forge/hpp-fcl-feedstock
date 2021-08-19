fb8cfe567b65c2d5d081950b23919221b359028d
Justin Carpentier <justin.carpentier@inria.fr>
2021-08-19 15:32:10 +0200

    build: simplify

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,16 +3,11 @@
 mkdir build
 cd build
 
-python -c "import lxml"
 ls $SP_DIR
 
-cmake .. \
+cmake ${CMAKE_ARGS} .. \
       -DCMAKE_BUILD_TYPE=Release \
       -DBUILD_PYTHON_INTERFACE=ON \
-      -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR=lib \
-      -DPython_EXECUTABLE=$PYTHON \
       -DPYTHON_EXECUTABLE=$PYTHON \
       -DBUILD_TESTING=OFF
 
