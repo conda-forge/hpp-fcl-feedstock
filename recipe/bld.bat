416e9d06558aaee6e837634a9e3a96cba458b842
Justin Carpentier <justin.carpentier@inria.fr>
2021-09-08 23:25:39 +0200

    recipe: enable stubs generation

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -25,3 +25,8 @@ if errorlevel 1 exit 1
 :: Install.
 cmake --build . --config Release --target install
 if errorlevel 1 exit 1
+
+:: Generate Stubs
+git clone https://github.com/jcarpent/pybind11-stubgen.git
+%PYTHON% "%CD%\pybind11-stubgen\pybind11_stubgen\__init__.py" -o %SP_DIR% hppfcl --boost-python --ignore-invalid signature --no-setup-py --root-module-suffix ""
+if errorlevel 1 exit 1
