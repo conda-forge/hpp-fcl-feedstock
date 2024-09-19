04c06a6a26f50447a6ac91eadb217dbbc02eb78b
Joris Vaillant <joris.vaillant@inria.fr>
2024-09-19 17:25:41 +0200

    Turn the package into a coal wrapper

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,3 @@
-set PKG_CONFIG_PATH=%LIBRARY_PREFIX%\share\pkgconfig
-
 mkdir build
 cd build
 
@@ -8,26 +6,17 @@ set "CXX=clang-cl.exe"
 set "CL=/MP"
 
 cmake ^
-    -G "NMake Makefiles" ^
+    -GNinja ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DPYTHON_SITELIB=%SP_DIR% ^
     -DPYTHON_EXECUTABLE=%PYTHON% ^
     -DBUILD_PYTHON_INTERFACE=ON ^
-    -DHPP_FCL_HAS_QHULL=ON ^
     -DBUILD_TESTING=OFF ^
+    -DCOAL_BACKWARD_COMPATIBILITY_WITH_HPP_FCL=ON ^
     %SRC_DIR%
 if errorlevel 1 exit 1
 
-:: Build.
-cmake --build . --config Release
-if errorlevel 1 exit 1
-
 :: Install.
-cmake --build . --config Release --target install
-if errorlevel 1 exit 1
-
-:: Generate Stubs
-git clone https://github.com/jcarpent/pybind11-stubgen.git
-%PYTHON% "%CD%\pybind11-stubgen\pybind11_stubgen\__init__.py" -o %SP_DIR% hppfcl --boost-python --ignore-invalid signature --no-setup-py --root-module-suffix ""
+ninja install-hpp-fcl-compatibility
 if errorlevel 1 exit 1
