e3cb799977a32d813581be90ee4660304942c846
Joris Vaillant <joris.vaillant@inria.fr>
2024-11-27 14:55:41 +0100

    Try to turn this package noarch

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,20 +3,7 @@
 mkdir build
 cd build
 
-export BUILD_NUMPY_INCLUDE_DIRS=$( $PYTHON -c "import numpy; print (numpy.get_include())")
-export TARGET_NUMPY_INCLUDE_DIRS=$SP_DIR/numpy/core/include
-
-echo $BUILD_NUMPY_INCLUDE_DIRS
-echo $TARGET_NUMPY_INCLUDE_DIRS
-
-if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
-  echo "Copying files from $BUILD_NUMPY_INCLUDE_DIRS to $TARGET_NUMPY_INCLUDE_DIRS"
-  mkdir -p $TARGET_NUMPY_INCLUDE_DIRS
-  cp -r $BUILD_NUMPY_INCLUDE_DIRS/numpy $TARGET_NUMPY_INCLUDE_DIRS
-  export Python3_NumPy_INCLUDE_DIR=$TARGET_NUMPY_INCLUDE_DIRS
-else
-  export Python3_NumPy_INCLUDE_DIR=$BUILD_NUMPY_INCLUDE_DIRS
-fi
+export Python3_NumPy_INCLUDE_DIR=$( $PYTHON -c "import numpy; print (numpy.get_include())")
 
 cmake ${CMAKE_ARGS} .. \
       -GNinja \
@@ -24,10 +11,9 @@ cmake ${CMAKE_ARGS} .. \
       -DPYTHON_EXECUTABLE=$PYTHON \
       -DGENERATE_PYTHON_STUBS=OFF \
       -DPython3_NumPy_INCLUDE_DIR=$Python3_NumPy_INCLUDE_DIR \
-      -DHPP_FCL_HAS_QHULL=ON \
-      -DCMAKE_CXX_STANDARD=11 \
       -DBUILD_PYTHON_INTERFACE=ON \
       -DBUILD_TESTING=OFF \
+      -DENABLE_PYTHON_DOXYGEN_AUTODOC=OFF \
       -DCOAL_BACKWARD_COMPATIBILITY_WITH_HPP_FCL=ON
 
 ninja install-hpp-fcl-compatibility
